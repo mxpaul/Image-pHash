ac733d3ba5963fc407089562476470d217e007eb
Maxim Polyakov <m.polyakov@corp.mail.ru>
2017-09-15 14:59:58 +0300

    fix core dump when no global color map found in gif image

--- readers/gif_reader.h
+++ readers/gif_reader.h
@@ -28,7 +28,7 @@ namespace image_reader {
 		char gif_header1[] = { 0x47, 0x49, 0x46, 0x38, 0x39, 0x61 };
 		char gif_header2[] = { 0x47, 0x49, 0x46, 0x38, 0x37, 0x61 };
 		const size_t gif_header_len = 6;
-		
+
 		if (strncmp(buffer, gif_header1, gif_header_len) == 0) {
 			return true;
 		} else if (strncmp(buffer, gif_header2, gif_header_len) == 0) {
@@ -36,15 +36,15 @@ namespace image_reader {
 		}
 		return false;
 	}
-	
-	
+
+
 #ifdef cimg_use_gif
 	struct GifDataSource {
 		const char* buf;
 		size_t len;
 		size_t pos;
 	};
-	
+
 	int gif_read(GifFileType *gif, GifByteType *out, int length) {
 		GifDataSource *src = (GifDataSource *) gif->UserData;
 		if (src == NULL) {
@@ -59,15 +59,15 @@ namespace image_reader {
 		if (length > bytes_left) {
 			length = bytes_left;
 		}
-		
+
 		memcpy(out, src->buf, length);
 		src->buf += length;
 		src->pos += length;
 		return length;
 	}
-	
+
 #endif
-	
+
 	template <typename T>
 	static CImg<T> *_read_gif(const char* buffer, size_t size) {
 #ifdef cimg_use_gif
@@ -75,7 +75,7 @@ namespace image_reader {
 		gif.buf = buffer;
 		gif.len = size;
 		gif.pos = 0;
-		
+
 		// warn("%d.%d.%d", GIFLIB_MAJOR, GIFLIB_MINOR, GIFLIB_RELEASE);
 		GifFileType *gif_file = DGifOpen(&gif, &gif_read);
 		int err = GifLastError();
@@ -83,30 +83,34 @@ namespace image_reader {
 			cwarn("Error while reading a gif image. Error = %d.", err);
 			return NULL;
 		}
-		
+
 		if (DGifSlurp(gif_file) == GIF_ERROR) {
 			cwarn("Error while reading a gif image. Error = %d", GifLastError());
 			return NULL;
 		}
-		
+
 		if (gif_file->ImageCount == 0) {
 			cwarn("No frames found in gif image");
 			return NULL;
 		}
-		
+
 		CImgList<T> cimgList;
 		cimgList.assign();
-		
+
 		try {
 			for (int i = 0; i < gif_file->ImageCount; ++i) {
 				SavedImage* img = &gif_file->SavedImages[i];
 				int width = img->ImageDesc.Width;
 				int height = img->ImageDesc.Height;
 				int depth = gif_file->SColorResolution;
-				int colors = gif_file->SColorMap->ColorCount;
+				int colors = gif_file->SColorMap == NULL ? 0 : gif_file->SColorMap->ColorCount;
 				int background = gif_file->SBackGroundColor;
 				ColorMapObject* color_map = img->ImageDesc.ColorMap ? img->ImageDesc.ColorMap : gif_file->SColorMap;
-				
+				/* if (color_map == NULL) { // no tests no code :-)
+					cwarn("No color map for gif image");
+					continue;
+				} */
+
 				ExtensionBlock* gcb = NULL;
 				for (int i = 0; i < img->ExtensionBlockCount; ++i) {
 					if (img->ExtensionBlocks[i].Function == GRAPHICS_EXT_FUNC_CODE) {
@@ -120,15 +124,15 @@ namespace image_reader {
 					transparent_color = gcb->Bytes[3];
 				}
 				// cwarn("width = %d; height = %d; depth = %d; colors = %d; background = %d; is_alpha = %d, transparent_color = %d", width, height, depth, colors, background, is_alpha, transparent_color);
-				
+
 				CImg<T> frame_img;
 				frame_img.assign(width, height, 1, 3 + (is_alpha?1:0));
-				
+
 				T *ptr_r = frame_img.data(0,0,0,0),
 				  *ptr_g = frame_img.data(0,0,0,1),
 				  *ptr_b = frame_img.data(0,0,0,2),
 				  *ptr_a = is_alpha ? frame_img.data(0,0,0,3) : 0;
-				
+
 				for (int y = 0; y < height; ++y) {
 					for (int x = 0; x < width; ++x) {
 						unsigned char color_idx = img->RasterBits[y * width + x];
@@ -137,7 +141,7 @@ namespace image_reader {
 						GifByteType g = color_obj.Green;
 						GifByteType b = color_obj.Blue;
 						GifByteType alpha = color_idx == (unsigned char) transparent_color ? 0x00 : 0xFF;
-						
+
 						switch (frame_img._spectrum) {
 						case 3: {
 							*(ptr_r++) = (T) r;
@@ -157,27 +161,27 @@ namespace image_reader {
 						}
 					}
 				}
-				
+
 				if (frame_img) {
 					frame_img.move_to(cimgList);
 				}
-				
+
 			}
 		} catch (...) {
 			if (!DGifCloseFile(gif_file)) {
 				cwarn("Error while closing a gif image. Error = %d", GifLastError());
 			}
-			
+
 			throw;
 		}
-		
+
 		if (!DGifCloseFile(gif_file)) {
 			cwarn("Error while closing a gif image. Error = %d", GifLastError());
 			return NULL;
 		}
-		
+
 		return new CImg<T>(cimgList.get_append('z', 0));
-		
+
 #else
 		return NULL;
 #endif
